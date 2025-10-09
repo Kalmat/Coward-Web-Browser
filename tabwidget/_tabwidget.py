5a82522555acf7b53d615a340d51f6033a755769
Kalmat <palookjones@gmail.com>
2025-10-09 23:01:13 +0200

    Improved resizing (horizontal) tabs

--- tabwidget/_tabwidget.py
+++ tabwidget/_tabwidget.py
@@ -1,5 +1,5 @@
 from PyQt6.QtGui import QFontMetrics
-from PyQt6.QtWidgets import QTabWidget, QTabBar
+from PyQt6.QtWidgets import QTabWidget, QTabBar, QStyleFactory
 
 from settings import DefaultSettings
 
@@ -19,11 +19,11 @@ class TabWidget(QTabWidget):
         if self.tabPosition() == QTabWidget.TabPosition.North:
             # button is not present, but pyqt6 reserves the space anyway
             button = self.tabBar().tabButton(index, QTabBar.ButtonPosition.RightSide)
-            b_width = 0 if button is None else (button.width() + 12)  # add padding too
-            available_width = self.width() - ((self.icon_size + b_width) * self.count())
-            tab_width = available_width / self.count()
-            if tab_width < DefaultSettings.Tabs.maxWidth - (self.icon_size + b_width):
-                target_length = int(tab_width / self.char_width)
+            b_width = 0 if button is None else (button.width() + 9)  # add padding too (empyrical)
+            available_width = max(0, self.width() - ((self.icon_size + b_width) * self.count()))
+            label_width = available_width / self.count()
+            if label_width < DefaultSettings.Tabs.maxWidth - (self.icon_size + b_width):
+                target_length = int(label_width / self.char_width)
             else:
                 target_length = len(self.tabText(index))
         else:
@@ -62,8 +62,8 @@ class TabWidget(QTabWidget):
     def setTabText(self, index, a1):
         target_text = a1
         if 0 < index < self.count() - 1:
-            text_length = self._getTextSize(index, a1)
-            target_text = a1[:text_length] + (" " * max(0, (text_length - len(a1))))
+            target_length = self._getTextSize(index, a1)
+            target_text = a1[:target_length] + (" " * max(0, (target_length - len(a1))))
         super().setTabText(index, target_text)
 
     def removeTab(self, index):
