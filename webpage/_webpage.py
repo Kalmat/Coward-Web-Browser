c77cd4672a17e7a5bee23919d0aeb4faee127126
Kalmat <palookjones@gmail.com>
2025-10-09 09:32:04 +0200

    added (experimental) more aggressive memory release feature for inactive tabs

--- webpage/_webpage.py
+++ webpage/_webpage.py
@@ -41,6 +41,7 @@ class WebPage(QWebEnginePage):
 
         self.mediaCheck = CheckMedia(self, self.isPlayingMediaSig, self.mediaErrorSig)
         self.mediaErrorSig.connect(self.handleMediaError)
+        self.windowCloseRequested.connect(self.onCloseRequested)
 
     # def acceptNavigationRequest(self, url, type, isMainFrame: bool) -> bool:
     #     if type == QWebEnginePage.NavigationType.NavigationTypeLinkClicked: return False
@@ -138,65 +139,5 @@ class WebPage(QWebEnginePage):
         )
         return dialog
 
-
-"""
-# TWITCH:
-# Error Level: JavaScriptConsoleMessageLevel.ErrorMessageLevel --- URL: ... --- Message: Player stopping playback - error Player:2 (ErrorNotSupported code 0 - No playable format) --- Line number: 1
-
-# YOUTUBE:
-# --> No error
-
-if level == WebEnginePage.JavaScriptConsoleMessageLevel.ErrorMessageLevel:
-    # this is totally empyrical and based in just one a small number of cases
-    # this is promising but... how to access this data?
-    # "debug_error": "{\"errorCode\":\"html5.unsupportedlive\",\"errorMessage\":\"No se admite este formato de vídeo.\",\"Ua\":\"HTML5_NO_AVAILABLE_FORMATS_FALLBACK\",\"Cx\":\"\",\"cN\":\"buildRej.1;a.1;d.1;drm.0;f18.0;c18.0;f133.1;f140.1;f242.1;cAAC.0;cAVC.0;cVP9.1;a6s.1\",\"Rl\":2,\"cpn\":\"KSAdNV_auzm2Ivhf\"}",
-
-    # this works for twitch, but not for YT live videos
-    if "Player" in message and "ErrorNotSupported" in message:
-        self.externalPlayer.handleExternalPlayerRequest(self.url().toString())       
-        
-Example for YouTube
-Here’s how you can set up an error listener using the YouTube IFrame Player API:
-
-<script src="https://www.youtube.com/iframe_api"></script>
-<div id="player"></div>
-<script>
-    var player;
-    
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            height: '390',
-            width: '640',
-            videoId: 'YOUR_VIDEO_ID',
-            events: {
-                'onError': onPlayerError,
-            }
-        });
-    }
-
-    function onPlayerError(event) {
-        console.log('Error detected:', event.data);
-        // Handle different error codes if needed
-    }
-</script>
-
-
-Example for Twitch
-For Twitch, you can set up an event listener like this:
-
-<script src="https://embed.twitch.tv/embed/v1.js"></script>
-<div id="twitch-embed"></div>
-<script type="text/javascript">
-    new Twitch.Embed("twitch-embed", {
-        channel: "YOUR_CHANNEL",
-        parent: ["YOUR_WEBSITE_DOMAIN"],
-        width: 800,
-        height: 600,
-    });
-
-    // Listen for errors
-    Twitch.on(Embed.Events.ERROR, function(error) {
-        console.log('Error detected:', error);
-    });
-</script>
-"""
+    def onCloseRequested(self):
+        self.mediaCheck.stop()
